a1f1836230bf0412dfd4bcd620d716829e9b6ae0
masthad404 <vogeldodo5@gmail.com>
2021-05-24 11:36:37 +0200

    initial commit

---
+++ client.py
@@ -0,0 +1,32 @@
+import socket
+
+HEADER = 64
+PORT = 5050
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.0.96"
+ADDR = (SERVER, PORT)
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+    message = msg.encode(FORMAT)
+    msg_length = len(message)
+    send_length = str(msg_length).encode(FORMAT)
+    send_length += b' ' * (HEADER - len(send_length))
+    client.send(send_length)
+    client.send(message)
+    print(client.recv(2048).decode(FORMAT))
+
+send("User connnected. Say hello!")
+
+keymessage = ""
+
+while not keymessage == "exit":
+    keymessage = input()
+    send(keymessage)
+
+
+send(DISCONNECT_MESSAGE)
+
